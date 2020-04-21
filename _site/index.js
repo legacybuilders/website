4bdf3b818ed44260d0ad3613cfceb569b3bc2609
metanomial <benjamin@metanomial.com>
2020-04-21 14:21:23 -0700

    Delete jekyll build files

--- _site/index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-	document.querySelector('#logobar .hamburger').addEventListener('click', event => {
-		event.preventDefault();
-		document.querySelector('#pagelinks').classList.toggle('expanded');
-	});
-});
